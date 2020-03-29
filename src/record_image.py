d904115e8080722f353bcc8275b643b7f4d653db
GoodField <goodfield055@gmail.com>
2020-03-29 11:25:03 +0900

    Add monitor functin

--- src/record_image.py
+++ src/record_image.py
@@ -5,12 +5,23 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 from datetime import datetime
 
+
+#
+# http://192.168.1.***:10000/stream?topic=cv_camera/image_raw  -> USBカメラ撮影画像（リアルタイム）
+#                     :10000/stream?topic=/picture             -> 記録画像（パラメータで指定）
+#                         rosparam set /picture_name "/tmp/image_20200329-11:05:45.jpg"
+#
+
+
 class RecordImage():
     def __init__(self):
-        sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.get_image)
+        self.pub = rospy.Publisher("picture", Image, queue_size=1)
+        self.sub = rospy.Subscriber("/cv_camera/image_raw", Image, self.get_image)
         self.bridge = CvBridge()
         self.image_org = None
         self.last_time = rospy.Time.now()
+        self.last_picture = ""
+        self.file_err_flag = False
 
     def get_image(self,img):
         try:
@@ -30,6 +41,22 @@ class RecordImage():
         
         return "saved"
 
+    def monitor_picture(self):
+        picture_name = rospy.get_param('picture_name',"/tmp/image.jpg")
+        
+        if self.last_picture != picture_name:
+            self.file_err_flag = False
+
+        if self.file_err_flag == False:
+            self.last_picture = picture_name
+            try:
+                img = cv2.imread(self.last_picture)
+                self.pub.publish(self.bridge.cv2_to_imgmsg(img, "bgr8"))        
+#               rospy.loginfo(self.last_picture)
+            except:
+                self.file_err_flag = True   # 一度読み込みエラー起こしたら２回目以降は読み込みしない
+                rospy.logerr("can't open image file(%s)", self.last_picture)
+
 if __name__ == '__main__':
     rospy.init_node("record_image")
     ri = RecordImage()
@@ -40,6 +67,8 @@ if __name__ == '__main__':
         if rospy.Time.now().to_sec() - ri.last_time.to_sec() >= 10.0:
             rospy.loginfo(ri.record_image())
             ri.last_time = rospy.Time.now()
+
+        ri.monitor_picture()
 
         rate.sleep()
 
